722eec9bf3c70b6b38cca6e4328f762384055a50
Meha Kaushik <mekaushi@microsoft.com>
2019-12-16 16:10:59 -0800

    Cmdlets for Azure CosmosDB Sql operations

---
+++ src/CosmosDB/CosmosDB/SQL/RemoveAzCosmosDBSqlStoredProcedure.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Text;
+using System.Linq;
+using Microsoft.Azure.Commands.CosmosDB.Models;
+using System.Reflection;
+using Microsoft.Rest.Azure;
+using Microsoft.Azure.Commands.ResourceManager.Common.ArgumentCompleters;
+using Microsoft.Azure.Commands.CosmosDB.Helpers;
+using Microsoft.Azure.Management.Internal.Resources.Utilities.Models;
+
+namespace Microsoft.Azure.Commands.CosmosDB
+{
+    [Cmdlet(VerbsCommon.Remove, ResourceManager.Common.AzureRMConstants.AzureRMPrefix + "CosmosDBSqlStoredProcedure", SupportsShouldProcess = true), OutputType( typeof(void), typeof(bool) )]
+    public class RemoveAzCosmosDBSqlStoredProcedure : AzureCosmosDBCmdletBase
+    {
+        [Parameter(Mandatory = false, ParameterSetName = NameParameterSet, HelpMessage = Constants.ResourceGroupNameHelpMessage)]
+        [ResourceGroupCompleter]
+        public string ResourceGroupName { get; set; }
+
+        [Parameter(Mandatory = false, ParameterSetName = NameParameterSet, HelpMessage = Constants.AccountNameHelpMessage)]
+        public string AccountName { get; set; }
+
+        [Parameter(Mandatory = false, ParameterSetName = NameParameterSet, HelpMessage = Constants.DatabaseNameHelpMessage)]
+        public string DatabaseName { get; set; }
+
+        [Parameter(Mandatory = false, HelpMessage = Constants.ContainerNameHelpMessage)]
+        public string ContainerName { get; set; }
+
+        [Parameter(Mandatory = false, HelpMessage = Constants.StoredProcedureNameHelpMessage)]
+        public string Name { get; set; }
+
+        [Parameter(Mandatory = false, HelpMessage = Constants.AsJobHelpMessage)]
+        public SwitchParameter AsJob { get; set; }
+
+        [Parameter(Mandatory = false, HelpMessage = Constants.PassThruHelpMessage)]
+        public SwitchParameter PassThru { get; set; }
+
+        public override void ExecuteCmdlet()
+        {
+            try
+            {
+                CosmosDBManagementClient.SqlResources.DeleteSqlStoredProcedureWithHttpMessagesAsync(ResourceGroupName, AccountName, DatabaseName, ContainerName, Name).GetAwaiter().GetResult();
+                
+                if (PassThru)
+                    WriteObject(bool.TrueString);
+            }
+            catch
+            {
+                if(PassThru)
+                    WriteObject(bool.FalseString);
+            }
+
+            return;
+        }
+    }
+}
---
+++ src/CosmosDB/CosmosDB/SQL/SetAzCosmosDBSqlStoredProcedure.cs
@@ -0,0 +1,78 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Management.Automation;
+using Microsoft.Azure.Commands.CosmosDB.Models;
+using Microsoft.Azure.Commands.ResourceManager.Common.ArgumentCompleters;
+using Microsoft.Azure.Commands.CosmosDB.Helpers;
+using Microsoft.Azure.Management.CosmosDB.Models;
+
+namespace Microsoft.Azure.Commands.CosmosDB
+{
+    [Cmdlet(VerbsCommon.Set, ResourceManager.Common.AzureRMConstants.AzureRMPrefix + "CosmosDBSqlStoredProcedure", DefaultParameterSetName = NameParameterSet), OutputType(typeof(PSSqlStoredProcedureGetResults))]
+    public class SetAzCosmosDBSqlStoredProcedure : AzureCosmosDBCmdletBase
+    {
+        [Parameter(Mandatory = false, ParameterSetName = NameParameterSet, HelpMessage = Constants.ResourceGroupNameHelpMessage)]
+        [ResourceGroupCompleter]
+        public string ResourceGroupName { get; set; }
+
+        [Parameter(Mandatory = false, ParameterSetName = NameParameterSet, HelpMessage = Constants.AccountNameHelpMessage)]
+        public string AccountName { get; set; }
+
+        [Parameter(Mandatory = false, ParameterSetName = NameParameterSet, HelpMessage = Constants.DatabaseNameHelpMessage)]
+        public string DatabaseName { get; set; }
+
+        [Parameter(Mandatory = false, ParameterSetName = NameParameterSet, HelpMessage = Constants.ContainerNameHelpMessage)]
+        public string ContainerName { get; set; }
+
+        [Parameter(Mandatory = false, HelpMessage = Constants.StoredProcedureNameHelpMessage)]
+        public string Name { get; set; }
+
+        [Parameter(Mandatory = false, HelpMessage = Constants.StoredProcedureBodyHelpMessage)]
+        public string Body { get; set; }
+
+        [Parameter(Mandatory = false, ParameterSetName = ParentObjectParameterSet, HelpMessage = Constants.SqlContainerObjectHelpMessage)]
+        public PSSqlContainerGetResults InputObject { get; set; }
+
+        [Parameter(Mandatory = false, HelpMessage = Constants.AsJobHelpMessage)]
+        public SwitchParameter AsJob { get; set; }
+
+        public override void ExecuteCmdlet()
+        {
+            //if(ParameterSetName.Equals(ParentObjectParameterSet))
+            //{
+            //    ResourceIdentifier resourceIdentifier = new ResourceIdentifier(InputObject.Id);
+            //    ResourceGroupName = resourceIdentifier.ResourceGroupName;
+            //    ContainerName = resourceIdentifier.ResourceName;
+            //    DatabaseName = resourceIdentifier.ParentResource;
+            //}
+
+            SqlStoredProcedureCreateUpdateParameters sqlStoredProcedureCreateUpdateParameters = new SqlStoredProcedureCreateUpdateParameters
+            {
+                Resource = new SqlStoredProcedureResource
+                {
+                    Id = Name,
+                    Body = Body
+                },
+                Options = new Dictionary<string, string>() { }
+            };
+
+            SqlStoredProcedureGetResults sqlStoredProcedureGetResults = CosmosDBManagementClient.SqlResources.CreateUpdateSqlStoredProcedureWithHttpMessagesAsync(ResourceGroupName, AccountName, DatabaseName, ContainerName, Name, sqlStoredProcedureCreateUpdateParameters).GetAwaiter().GetResult().Body;
+            WriteObject(new PSSqlStoredProcedureGetResults(sqlStoredProcedureGetResults));
+
+            return;
+        }
+    }
+}
